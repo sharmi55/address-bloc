492a6391f767fa36eb4c9327b63b44c02e16172a
Sharmi <rai.sharmistha@gmail.com>
2016-02-22 10:58:24 +0100

    Handle user input set up with stub methods

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -3,7 +3,7 @@ require_relative '../models/address_book.rb'
 class MenuController
   attr_accessor :address_book
 
-  def intialize
+  def initialize
     @address_book = AddressBook.new
   end
 
@@ -18,6 +18,43 @@ class MenuController
     print "Enter your selection: "
 
     selection = gets.to_i
-    puts "You picked #{selection}"
+
+    case selection
+    when 1
+      system "clear"
+      view_all_entries
+      main_menu
+    when 2
+      system "clear"
+      create_entry
+      main_menu
+    when 3
+      system "clear"
+      search_entries
+      main_menu
+    when 4
+      system "clear"
+      read_csv
+      main_menu
+    when 5
+      puts "Good-bye!"
+
+      exit(0)
+
+    else
+      system "clear"
+      puts "Sorry, that is not a valid input"
+      main_menu
+    end
   end
+
+    def view_all_entries
+    end
+
+    def create_entry
+    end
+
+    def read_csv
+    end
+
 end
